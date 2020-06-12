c0def358a54dfe1f0f719c1aebfdc38014f73a0c
Rami Alhussein <Ramialhussein98@gmail.com>
2020-06-12 17:54:43 -0400

    Updates

--- .sequelizerc.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    "Config" : path.resolve('./config', 'config.js'),
-    "modules-path": path.resolve('./models'),
-    "seeders-path": path.resolve('./seeders'),
-    "migrations-path": path.resolve('./migrations')
-};
--- tests/hash.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require("bcryptjs");
-const passwords = process.argv.slice(2);
-const hashes = [];
-passwords.map(pw => {
-    hashes.push({ "pw": pw, "hash": bcrypt.hashSync(pw, 10) });
-})
-console.log(hashes);
